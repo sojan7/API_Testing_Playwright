7627b972ed8a7e183116112b6946c3820d64f0c2
Sojan Somarajan <sojan_somarajan@epam.com>
2024-08-10 21:03:17 +0530

    Sample API Call added

--- tests/rest_api_testing.spec.js
+++ tests/rest_api_testing.spec.js
@@ -1,28 +1,9 @@
 import { test, expect } from "@playwright/test";
-import { defineConfig } from "@playwright/test";
 
-test("Create a new user and verify response", async ({
-  request,
-  defineConfig,
-}) => {
+test("Create a new user and verify response", async ({ request }) => {
   // Use the base URL defined in the config
-  const response = await request.post("/v2/users", {
-    // Headers are automatically set by the configuration
-    data: {
-      name: "John Doe",
-      gender: "male",
-      status: "active",
-      email: "johndoe@example.com",
-    },
-  });
+  const response = await request.get("https://gorest.co.in/");
+  expect(response.ok()).toBeTruthy();
+  expect(response.status()).toBe(200);
   console.log(response);
-
-  // Verify the response status and body
-  expect(response.status()).toBe(201);
-  const responseBody = await response.json();
-  expect(responseBody).toHaveProperty(
-    "message",
-    "Resource created successfully."
-  );
-  expect(responseBody).toHaveProperty("id"); // Assuming 'id' is returned
 });
